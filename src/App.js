cfb7687c02653efdad064c3288f7831d8ad995fc
Frederico Jordan <fredericojordan@gmail.com>
2018-05-09 20:08:24 -0300

    Adding video-thumb

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
 import Navbar from './components/navbar'
+import VideoThumb from './components/video-thumb'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+          <div style={{width: '300px', height: '200px',}}>
+            <VideoThumb url="https://cdn-images-1.medium.com/max/800/1*0te_tttSd-oWbW4v1wHXEw.jpeg"/>
+          </div>
       </div>
     )
   }
