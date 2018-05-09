bd99b9a30e22649867f19937f8acabf2158a4796
Frederico Jordan <fredericojordan@gmail.com>
2018-05-09 19:14:46 -0300

    Adding initial navbar

---
+++ src/components/navbar/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Navbar = props => (
+    <nav className="navbar">
+        <span className="logo">
+            Choque de Cultura
+        </span>
+    </nav>
+)
+
+
+export default Navbar
